9dc25f04356cf7f4394f2fad298f82b403371063
Kim MooWon <anselmo12@naver.com>
2024-03-13 12:48:54 +0900

    공룡 정보 추가

--- src/main/java/com/example/dinosaurpark/DinosaurController.java
+++ src/main/java/com/example/dinosaurpark/DinosaurController.java
@@ -1,33 +1,24 @@
 package com.example.dinosaurpark;
 
-//프로젝트 내 필요한 클래스 임포트
-import com.example.dinosaurpark.Dinosaur;
-
-//스프링부트 라이브러리 임포트
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
-
-//자바 라이브러리 임포트
-import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DinosaurController {
     
-    @GetMapping("/dino")
-    public String listDinosaurs(Model model) {
-        List<Dinosaur> dinosaurs = createDinosaurs();
-        model.addAttribute("dinosaurs", dinosaurs);
-        return "dinosaurList";
-    }
+	private final DinosaurService dinosaurService;
 
-    private List<Dinosaur> createDinosaurs() {
-        List<Dinosaur> dinosaurs = new ArrayList<>();
-        dinosaurs.add(new Dinosaur("Tyrannosaurus", "Type 1"));
-        dinosaurs.add(new Dinosaur("Triceratops", "Type 2"));
-        //필요한 공룡 더 추가하기
-
-        return dinosaurs;
-    }
+	public DinosaurController(DinosaurService dinosaurService) {
+		this.dinosaurService = dinosaurService;
+	}
+	
+	@GetMapping("/dinosaurs")
+	public String listDinosaurs(Model model) {
+		List<Dinosaur> dinosaurs = dinosaurService.getAllDinosaurs();
+		model.addAttribute("dinosaurs", dinosaurs);
+		return "dinosaurList";
+	}
 }
