ce30ec5c918a4001f203ae83145a5093a164297b
Kim MooWon <anselmo12@naver.com>
2024-03-14 15:03:59 +0900

    직원 부서 및 프로필 관리 추가

--- src/main/resources/static/js/empProfile.js
+++ src/main/resources/static/js/empProfile.js
@@ -1,20 +1,24 @@
-const buttons = document.querySelectorAll(".card-buttons button");
-const sections = document.querySelectorAll(".card-section");
-const card = document.querySelector(".card");
+document.querySelectorAll(".card").forEach(card => {
+  const buttons = card.querySelectorAll(".card-buttons button");
+  const sections = card.querySelectorAll(".card-section");
 
-const handleButtonClick = (e) => {
-  const targetSection = e.target.getAttribute("data-section");
-  const section = document.querySelector(targetSection);
-  targetSection !== "#about"
-    ? card.classList.add("is-active")
-    : card.classList.remove("is-active");
-  card.setAttribute("data-state", targetSection);
-  sections.forEach((s) => s.classList.remove("is-active"));
-  buttons.forEach((b) => b.classList.remove("is-active"));
-  e.target.classList.add("is-active");
-  section.classList.add("is-active");
-};
+  buttons.forEach(btn => {
+    btn.addEventListener("click", e => {
+      const targetSection = e.target.getAttribute("data-section");
+      const section = card.querySelector(targetSection);
+      
+      if (targetSection !== "#about") {
+        card.classList.add("is-active");
+      } else {
+        card.classList.remove("is-active");
+      }
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", handleButtonClick);
+      card.setAttribute("data-state", targetSection);
+      sections.forEach(s => s.classList.remove("is-active"));
+      buttons.forEach(b => b.classList.remove("is-active"));
+      
+      e.target.classList.add("is-active");
+      section.classList.add("is-active");
+    });
+  });
 });
