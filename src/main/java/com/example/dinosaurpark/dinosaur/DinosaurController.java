08b7fb787ad4029cc6e94a5180aadcbe57d33f0f
Darkingy <jiwoohue@gmail.com>
2024-03-16 17:35:47 +0900

    reconstruction of project

--- src/main/java/com/example/dinosaurpark/dinosaur/DinosaurController.java
+++ src/main/java/com/example/dinosaurpark/dinosaur/DinosaurController.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.Map;
 
+//RequestMapping으로 공룡 페이지의 베이스가 되는 /dinosaurs가 무조건 앞에 나오게 되므로
+//다른 페이지를 매핑할 땐 /dinosaurs는 빼고, 그 뒤에 따르는 매핑만 적용시키면 됨
+@RequestMapping("/dinosaurs")
 @Controller
 public class DinosaurController {
     
@@ -17,21 +20,8 @@ public class DinosaurController {
         this.dinosaurService = dinosaurService;
     }
     
-    @GetMapping("/dinosaurs")
-    public String listDinosaurs(Model model) {
-        Map<String, List<Dinosaur>> dinosaurMap = dinosaurService.getDinosaursByType();
-        model.addAttribute("dinosaurMap", dinosaurMap);
-        return "dinosaurList";
-    }
-    
-	
-	@GetMapping("/dinosaurs/details/{id}") public String dinosaurDetails(Model
-	model, @PathVariable("id") Integer id) { Dinosaur dinosaur =
-	dinosaurService.getDinosaurById(id); model.addAttribute("dinosaur",
-	dinosaur); return "dinosaur_details"; }
-	 
-    
-    @GetMapping("/dinosaurs/type/{type}")
+    //예를 들어, 이페이지는 이제 자동으로 /dinosaurs/type/{type}으로 넘어가짐
+    @GetMapping(value = "/type/{type}")
     public String listDinosaursByType(Model model, @PathVariable("type") String type) {
         List<Dinosaur> dinosaurs = dinosaurService.getDinosaursByType().get(type);
         model.addAttribute("dinosaurs", dinosaurs); // 공룡 정보를 모델에 추가
