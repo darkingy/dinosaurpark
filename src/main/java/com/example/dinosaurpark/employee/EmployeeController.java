facccb1b282724f763fc6d87904399e010debe31
Kim MooWon <anselmo12@naver.com>
2024-03-18 10:10:32 +0900

    파일 정리 및 직원 정보 하나의 html로

--- src/main/java/com/example/dinosaurpark/employee/EmployeeController.java
+++ src/main/java/com/example/dinosaurpark/employee/EmployeeController.java
@@ -3,7 +3,11 @@ package com.example.dinosaurpark.employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @RequestMapping("/employee")
 @Controller
@@ -15,6 +19,15 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
     
+@GetMapping("/empdepart/{emptype}")
+public String empProfile(Model model, @PathVariable("emptype") String emptype) {
+    List<Employee> employees = employeeService.getEmployeeByType().get(emptype);
+    model.addAttribute("employees", employees);
+    model.addAttribute("emptype", emptype);
+    return "empProfile";
+}
+
+
     @GetMapping("/zookeeper")
     public String zooKeeper(Model model) {
         //TODO 각 모델에 따른 message가 적용되는 것 같지 않은데, 필요하지 않으면 삭제 부탁!
